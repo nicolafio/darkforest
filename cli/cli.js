6ae1307c9e01f2fc2ceb2240a30bcadd1288573c
Nicola Fiori (JD342) <nicola@jd342.net>
2023-10-29 18:34:50 +0000

    Format

--- cli/cli.js
+++ cli/cli.js
@@ -1,6 +1,6 @@
 const { unlink } = require('fs/promises');
 const { argv, stdin, stdout, exit } = require('process');
-const { once } = require('events')
+const { once } = require('events');
 const { join } = require('path');
 const puppeteer = require('puppeteer');
 const readline = require('readline');
@@ -19,244 +19,242 @@ const GAME_TERMINAL_INPUT_CSS_SELECTOR = '[class*="Terminal"] textarea';
 const REQUEST_PRIVATE_KEY_COMMAND = 'id';
 
 async function main() {
-    const {host, port, privateKey, requestingPrivateKey} = getParameters();
+  const { host, port, privateKey, requestingPrivateKey } = getParameters();
 
-    if (requestingPrivateKey) {
-        console.log(privateKey);
-        return;
-    }
+  if (requestingPrivateKey) {
+    console.log(privateKey);
+    return;
+  }
 
-    const privKeyBuf = Buffer.from(stripHexPrefix(privateKey), 'hex');
-    const userAddress = addHexPrefix(bufferToHex(privateToAddress(privKeyBuf)));
-    const userDataDir = join(DATA_DIR, userAddress);
+  const privKeyBuf = Buffer.from(stripHexPrefix(privateKey), 'hex');
+  const userAddress = addHexPrefix(bufferToHex(privateToAddress(privKeyBuf)));
+  const userDataDir = join(DATA_DIR, userAddress);
 
-    try {
-        // https://github.com/puppeteer/puppeteer/issues/10517
-        await unlink(join(userDataDir, 'SingletonLock'));
-    }
-    catch (e) {
-        if (e.code !== 'ENOENT') throw e;
-    }
+  try {
+    // https://github.com/puppeteer/puppeteer/issues/10517
+    await unlink(join(userDataDir, 'SingletonLock'));
+  } catch (e) {
+    if (e.code !== 'ENOENT') throw e;
+  }
 
-    const browser = await puppeteer.launch({
-        headless: 'new',
-        userDataDir,
-        args: ['--no-sandbox']
-    });
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    userDataDir,
+    args: ['--no-sandbox'],
+  });
 
-    process.on('exit', (code) => {
-        browser.close()
-            .catch(console.error)
-            .finally(() => { process.exit(code); })
-    });
+  process.on('exit', (code) => {
+    browser
+      .close()
+      .catch(console.error)
+      .finally(() => {
+        process.exit(code);
+      });
+  });
 
-    const page = await browser.newPage();
+  const page = await browser.newPage();
 
-    await navigateToLandingPage(page, host, port);
-    await enterInGameRound(page);
+  await navigateToLandingPage(page, host, port);
+  await enterInGameRound(page);
 
-    handleUnknownErrors(page).catch(handleAsFatalError);
-    handleOutOfMoneyCase(page).catch(handleAsFatalError);
-    handleGameJoinFail(page).catch(handleAsFatalError);
+  handleUnknownErrors(page).catch(handleAsFatalError);
+  handleOutOfMoneyCase(page).catch(handleAsFatalError);
+  handleGameJoinFail(page).catch(handleAsFatalError);
 
-    await connectToDarkForestContract(page, privateKey);
+  await connectToDarkForestContract(page, privateKey);
 
-    console.log('Connected to Dark Forest');
+  console.log('Connected to Dark Forest');
 
-    await forwardGameTerminalOutputToStdout(page);
+  await forwardGameTerminalOutputToStdout(page);
 
-    const sideTasksControl = new AbortController();
+  const sideTasksAC = new AbortController();
 
-    handleMissingHomeCoords(page, sideTasksControl.signal)
-        .catch(handleAsFatalError);
+  handleMissingHomeCoords(page, sideTasksAC.signal).catch(handleAsFatalError);
 
-    findHomePlanetIfAsked(page, sideTasksControl.signal)
-        .catch(handleAsFatalError);
+  findHomePlanetIfAsked(page, sideTasksAC.signal).catch(handleAsFatalError);
 
-    await page.waitForSelector(
-        'text/Press ENTER to begin',
-        {timeout: 0}
-    );
-        
-    sideTasksControl.abort();
-    await page.focus(GAME_TERMINAL_INPUT_CSS_SELECTOR);
-    await page.keyboard.press('Enter');
+  await page.waitForSelector('text/Press ENTER to begin', { timeout: 0 });
 
-    const input = readline.createInterface(stdin);
+  sideTasksAC.abort();
+  await page.focus(GAME_TERMINAL_INPUT_CSS_SELECTOR);
+  await page.keyboard.press('Enter');
 
-    while (true) {
-        stdout.write('> ');
-        const line = await once(input, 'line');
-        clearStdoutLine();
-        await inputLineToGameTerminal(page, line);
-    }
+  const input = readline.createInterface(stdin);
+
+  while (true) {
+    stdout.write('> ');
+    const line = await once(input, 'line');
+    clearStdoutLine();
+    await inputLineToGameTerminal(page, line);
+  }
 }
 
 async function navigateToLandingPage(page, host, port) {
-    const landingPageURL = `http://${host}:${port}`;
-    console.log(`Navigating to ${landingPageURL}...`);
-    let waitTime = 0;
-    const maxWaitTime = 1000;
-    while (true) {
-        try {
-            await page.goto(landingPageURL);
-            return;
-        }
-        catch (e) {
-            if (e.code !== 'ECONNREFUSED' &&
-                !String(e).includes('CONNECTION_REFUSED'))
-                throw e;
-        }
-        await new Promise((res) => {setTimeout(res, waitTime);});
-        waitTime *= 2;
-        if (waitTime === 0) waitTime = 1;
-        if (waitTime > maxWaitTime) waitTime = maxWaitTime;
+  const landingPageURL = `http://${host}:${port}`;
+  console.log(`Navigating to ${landingPageURL}...`);
+  let waitTime = 0;
+  const maxWaitTime = 1000;
+  while (true) {
+    try {
+      await page.goto(landingPageURL);
+      return;
+    } catch (e) {
+      if (!isConnectionRefusedError(e)) throw e;
     }
+    await new Promise((res) => {
+      setTimeout(res, waitTime);
+    });
+    waitTime *= 2;
+    if (waitTime === 0) waitTime = 1;
+    if (waitTime > maxWaitTime) waitTime = maxWaitTime;
+  }
+}
+
+function isConnectionRefusedError(error) {
+  if (error.code === 'ECONNREFUSED') return true;
+  if (String(error).includes('CONNECTION_REFUSED')) return true;
+  return false;
 }
 
 async function enterInGameRound(page) {
-    const btn = await page.waitForSelector('df-button::-p-text("Enter Round")');
-    await Promise.all([page.waitForNavigation(), btn.click()]);
+  const btn = await page.waitForSelector('df-button::-p-text("Enter Round")');
+  await Promise.all([page.waitForNavigation(), btn.click()]);
 }
 
 async function connectToDarkForestContract(page, privateKey) {
-    await page.waitForSelector('text/(i) Import private key.');
-    await page.waitForSelector(GAME_TERMINAL_INPUT_CSS_SELECTOR);
-    await inputLineToGameTerminal(page, 'i');
+  await page.waitForSelector('text/(i) Import private key.');
+  await page.waitForSelector(GAME_TERMINAL_INPUT_CSS_SELECTOR);
+  await inputLineToGameTerminal(page, 'i');
 
-    await page.waitForSelector('text/Enter the 0x-prefixed private key');
-    await inputLineToGameTerminal(page, privateKey)
+  await page.waitForSelector('text/Enter the 0x-prefixed private key');
+  await inputLineToGameTerminal(page, privateKey);
 
-    await page.waitForSelector('text/Connected to Dark Forest');
+  await page.waitForSelector('text/Connected to Dark Forest');
 }
 
 async function handleMissingHomeCoords(page, signal) {
-    try {
-        await waitForSelectorForever(
-            page,
-            'text/Import account home coordinates? (y/n)',
-            { signal }
-        );
-    }
-    catch (e) {
-        if (e.name !== 'AbortError') throw e;
-    }
-    if (signal.aborted) return;
-    console.error('Home coordinates missing. Have you deleted session data?');
-    console.error('Terminating.')
-    exit(1);
+  const importKeyMsg = 'Import account home coordinates? (y/n)';
+  try {
+    await waitForSelectorForever(page, 'text/' + importKeyMsg, { signal });
+  } catch (e) {
+    if (e.name !== 'AbortError') throw e;
+  }
+  if (signal.aborted) return;
+  console.error('Home coordinates missing. Have you deleted session data?');
+  console.error('Terminating.');
+  exit(1);
 }
 
 async function findHomePlanetIfAsked(page, signal) {
-    try {
-        await waitForSelectorForever(
-            page,
-            'text/Press ENTER to find a home planet.',
-            { signal }
-        );
-    }
-    catch (e) {
-        if (e.name !== 'AbortError') throw e;
-    }
-    if (signal.aborted) return;
-    await page.focus('textarea');
-    await page.keyboard.press('Enter');
+  const findHomePlanetMsg = 'Press ENTER to find a home planet.';
+  try {
+    await waitForSelectorForever(page, `text/` + findHomePlanetMsg, { signal });
+  } catch (e) {
+    if (e.name !== 'AbortError') throw e;
+  }
+  if (signal.aborted) return;
+  await page.focus('textarea');
+  await page.keyboard.press('Enter');
 }
 
 async function forwardGameTerminalOutputToStdout(page) {
-    await page.exposeFunction('DF_CLI_PRINT', (str) => { stdout.write(str); });
-    await page.exposeFunction('DF_CLI_RM_LINE', () => { clearStdoutLine(); });
-    await page.exposeFunction('DF_CLI_NEW_LINE', () => { stdout.write('\n'); });
-    await page.evaluate(() => {
-        const outputEmitter = window.df.terminal.current.getOutputEmitter();
-        outputEmitter.on('ON_OUTPUT', (output) => {
-            if (output.type === 'print') window['DF_CLI_PRINT'](output.str);
-            if (output.type === 'remove-line') window['DF_CLI_RM_LINE']()
-            if (output.type === 'new-line') window['DF_CLI_NEW_LINE']();
-        });
+  await page.exposeFunction('DF_CLI_PRINT', (str) => {
+    stdout.write(str);
+  });
+  await page.exposeFunction('DF_CLI_RM_LINE', () => {
+    clearStdoutLine();
+  });
+  await page.exposeFunction('DF_CLI_NEW_LINE', () => {
+    stdout.write('\n');
+  });
+  await page.evaluate(() => {
+    const outputEmitter = window.df.terminal.current.getOutputEmitter();
+    outputEmitter.on('ON_OUTPUT', (output) => {
+      if (output.type === 'print') window['DF_CLI_PRINT'](output.str);
+      if (output.type === 'remove-line') window['DF_CLI_RM_LINE']();
+      if (output.type === 'new-line') window['DF_CLI_NEW_LINE']();
     });
+  });
 }
 
 async function inputLineToGameTerminal(page, line) {
-    await page.focus(GAME_TERMINAL_INPUT_CSS_SELECTOR);
-    await page.type(GAME_TERMINAL_INPUT_CSS_SELECTOR, line)
-    await page.keyboard.press('Enter')
+  await page.focus(GAME_TERMINAL_INPUT_CSS_SELECTOR);
+  await page.type(GAME_TERMINAL_INPUT_CSS_SELECTOR, line);
+  await page.keyboard.press('Enter');
 }
 
 function clearStdoutLine() {
-    stdout.moveCursor(0, -1)
-    stdout.clearLine(1)
+  stdout.moveCursor(0, -1);
+  stdout.clearLine(1);
 }
 
 function handleAsFatalError(err) {
-    console.error(err);
-    process.exit(1);
+  console.error(err);
+  process.exit(1);
 }
 
 function getParameters() {
-    const args = argv.slice(2);
-    let privateKey;
+  const args = argv.slice(2);
+  let privateKey;
 
-    const playerIndex = args.find(a => /[0-9]+/.test(a));
-    const requestingPrivateKey = args.includes(REQUEST_PRIVATE_KEY_COMMAND);
+  const playerIndex = args.find((a) => /[0-9]+/.test(a));
+  const playerIndexGiven = typeof playerIndex === 'string';
+  const requestingPrivateKey = args.includes(REQUEST_PRIVATE_KEY_COMMAND);
 
-    if (typeof playerIndex === 'string')
-        privateKey = getPrivateKeyFromPlayerIndex(playerIndex);
-    if (!privateKey)
-        privateKey = args.find(a => a.startsWith('0x'));
-    if (!privateKey)
-        privateKey = getPrivateKeyFromPlayerIndex(0);
+  if (playerIndexGiven) privateKey = getPrivateKeyFromPlayerIndex(playerIndex);
+  if (!privateKey) privateKey = args.find((a) => a.startsWith('0x'));
+  if (!privateKey) privateKey = getPrivateKeyFromPlayerIndex(0);
 
-    const [host, port] =
-        args.find(a => a.includes(':'))?.split(':') ||
-        [DEFAULT_HTTP_CLIENT_HOST, DEFAULT_HTTP_CLIENT_PORT]
+  const [host, port] = args.find((a) => a.includes(':'))?.split(':') || [
+    DEFAULT_HTTP_CLIENT_HOST,
+    DEFAULT_HTTP_CLIENT_PORT,
+  ];
 
-    return {
-        privateKey,
-        host,
-        port,
-        requestingPrivateKey
-    };
+  return {
+    privateKey,
+    host,
+    port,
+    requestingPrivateKey,
+  };
 }
 
 function getPrivateKeyFromPlayerIndex(index) {
-    return addHexPrefix(
-        HDWallet.fromMnemonic(HD_KEYS_MNEMONIC)
-            .derive(HD_KEYS_PATH)
-            .derive(index)
-            .getPrivateKey()
-            .toString('hex')
-    );
+  return addHexPrefix(
+    HDWallet.fromMnemonic(HD_KEYS_MNEMONIC)
+      .derive(HD_KEYS_PATH)
+      .derive(index)
+      .getPrivateKey()
+      .toString('hex')
+  );
 }
 
 async function handleUnknownErrors(page) {
-    await waitForSelectorForever(page, 'text/An unknown error occurred.')
-    handleAsFatalError('Unkown error occurred');
+  await waitForSelectorForever(page, 'text/An unknown error occurred.');
+  handleAsFatalError('Unkown error occurred');
 }
 
-async function handleOutOfMoneyCase(page) { 
-    await waitForSelectorForever(page, 'text/xDAI balance too low!');
-    handleAsFatalError('Out of money. Terminating');
+async function handleOutOfMoneyCase(page) {
+  await waitForSelectorForever(page, 'text/xDAI balance too low!');
+  handleAsFatalError('Out of money. Terminating');
 }
 
 async function handleGameJoinFail(page) {
-    await waitForSelectorForever(page, 'text/Error Joining Game:')
-    handleAsFatalError('Cloud not join game. Terminating');
+  await waitForSelectorForever(page, 'text/Error Joining Game:');
+  handleAsFatalError('Cloud not join game. Terminating');
 }
 
 async function waitForSelectorForever(page, selector, options = {}) {
-    while (true) {
-        try {
-            return await page.waitForSelector(selector, {
-                timeout: 0,
-                ...options
-            });
-        }
-        catch (e) {
-            const isTimeoutError = e.message?.includes('timed out');
-            if (!isTimeoutError) throw e; 
-        }
+  while (true) {
+    try {
+      return await page.waitForSelector(selector, {
+        timeout: 0,
+        ...options,
+      });
+    } catch (e) {
+      const isTimeoutError = e.message?.includes('timed out');
+      if (!isTimeoutError) throw e;
     }
+  }
 }
 
-main().catch(console.error)
+main().catch(console.error);
